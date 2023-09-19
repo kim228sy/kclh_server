a02973bc81eab5c539e552888c64aedaa79e4400
kim228sy <kim228sy@gmail.com>
2023-09-19 18:09:13 +0900

    수정

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
-const employeeRouter = require('./employee');
-const departmentRouter = require('./department');
-const plcRouter = require('./plc');
-
+const employeeRouter = require("./employee");
+const departmentRouter = require("./department");
+// const plcRouter = require("./plc");
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Express" });
 });
-router.use('/employee', employeeRouter);
+router.use("/employee", employeeRouter);
 // router.use('/department', departmentRouter);
-router.use('/', plcRouter);
+// router.use('/', plcRouter);
 
 module.exports = router;
